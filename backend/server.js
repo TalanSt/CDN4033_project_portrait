40162c9d47d6fce789ce9df127225ba25aff53cc
Talan122 <yellowramairta@gmail.com>
2026-06-18 14:03:27 -0400

    Implement user table

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const https = require("https");
 const path = require("path");
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 let privateKey = fs.readFileSync("../key.pem", "utf-8");
 let certificate = fs.readFileSync("../cert.pem", "utf-8");
@@ -10,6 +10,7 @@ let certificate = fs.readFileSync("../cert.pem", "utf-8");
 let credentials = { key: privateKey, cert: certificate };
 
 const express = require("express");
+const { exit } = require("process");
 const app = express();
 
 const httpPort = 3000;
@@ -50,8 +51,19 @@ try {
 }
 catch (error) {
     console.error("Could not connect to database:", error);
+    exit();
 }
 
+const user = sequelize.define( "User",
+    {
+        id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+        username: {type: DataTypes.STRING, allowNull: false, unique: true},
+        password: {type: DataTypes.STRING, allowNull: false}
+    }
+);
+
+
+user.sync({ force: true });
 
 console.log("Listening for HTTP and HTTPS calls.");
 
